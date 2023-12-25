56933ebc0768f096b8c2103639132f3b0db2e718
jadavnishith <mrnick076@gmail.com>
2023-12-25 11:37:13 +0530

    constant

---
+++ index.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{
+    //value reassign na kri skiye
+    //constant variable fixed value
+    
+    const int a=10;
+    printf("%d\n",a);
+    
+    const float f=10.34;
+    printf("%f\n",f);
+    
+    const char c='n';
+    printf("%c\n",c);
+    return 0;
+    
+}
